022c35239212ced9131d92abdcd6f0021dfe8257
Joyce Bosco <joycegodinhobosco@gmail.com>
2021-07-17 21:07:59 -0300

    add post implemented

--- app/Controllers/Http/PostController.js
+++ app/Controllers/Http/PostController.js
@@ -13,6 +13,33 @@ class PostController {
             title: 'Latest Posts',
             posts: posts.toJSON()
         })
+    }
+
+    async details({ params, view }){
+        const post = await Post.find(params.id)
+        console.log(post)
+
+        return view.render('posts.details', {
+            post: post
+        })
+
+    }
+
+    async add({ view }){
+        return view.render('posts.add')
+    }
+
+    async store ({ request, response, session }) {
+        const post = new Post()
+
+        post.title = request.input('title')
+        post.body = request.input('body')
+
+        await post.save()
+
+        session.flash({ notification: 'Post Added' })
+
+        return response.redirect('/posts')
 
     }
 }
--- start/routes.js
+++ start/routes.js
@@ -19,3 +19,12 @@ const Route = use('Route')
 Route.on('/').render('home')
 
 Route.get('/posts', 'PostController.index')
+
+Route.get('/posts/add', 'PostController.add')
+
+Route.get('/posts/:id', 'PostController.details')
+
+Route.post('/posts', 'PostController.store')
+
+
+
